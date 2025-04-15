5f63167a781301855b03201a73248273d849cc01
jaisharma687 <jaisharma687@outlook.com>
2025-04-16 01:08:50 +0530

    deployment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import OfferHeader from './components/OfferHeader'
 import Navbar from './components/Navbar'
